7e73524996fa116827e0dd848fbe9fce465f7697
Virgil Ghizelea <vigh@gmx.de>
2022-09-11 13:23:27 +0200

    Initial commit

---
+++ src/main/java/vg/legoScore/LegoScoreApplication.java
@@ -0,0 +1,14 @@
+package vg.legoScore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoScoreApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(LegoScoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vg/legoScore/LegoScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package vg.legoScore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
