14d80f4aa75154ad6f917ebdf8bed055a9d24ed7
Virgil Ghizelea <vigh@gmx.de>
2022-09-12 20:45:01 +0200

    Erster Webservice Aufruf und Klasse PartCategory hinzugefügt

--- src/main/java/vg/legoScore/LegoScoreApplication.java
+++ src/main/java/vg/legoScore/LegoScoreApplication.java
@@ -1,14 +1,34 @@
 package vg.legoScore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.Part;
 
 @SpringBootApplication
 public class LegoScoreApplication {
 
-	public static void main(String[] args) {
+	private static final Logger log = LoggerFactory.getLogger(LegoScoreApplication.class);
 
-		SpringApplication.run(LegoScoreApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(LegoScoreApplication.class, args);}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			PartCategory partCategory = restTemplate.getForObject(
+			"https://rebrickable.com/api/v3/lego/part_categories/12/?key=d4f0a3eaa0fc59ffc6f425289e8640c2", PartCategory.class);
+			log.info(partCategory.toString());
+		};
+	}
 }
---
+++ src/main/java/vg/legoScore/PartCategory.java
@@ -0,0 +1,33 @@
+package vg.legoScore;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PartCategory {
+
+    private Long id;
+    private String name;
+    private Long part_count;
+
+    public PartCategory() {
+    }
+
+    public Long getId() {return this.id;}
+    public void setId(Long id) {this.id=id;}
+
+    public String getName() {return this.name;}
+    public void setName(String name) {this.name = name;}
+
+    public Long getPart_count() {return part_count;}
+
+    public void setPart_count(Long part_count) {this.part_count = part_count;}
+
+    @Override
+    public String toString() {
+        return "PartCategory{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", part_count='" + part_count + '\'' +
+                '}';
+    }
+}
